a01f3c821a8b22e249ecf1ec45b45abd40283711
sahil <igsahilsayyad>
2024-06-03 06:44:13 +0530

    task completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,61 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+
+const Tooltip = ({ children, position, content }) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  const handleMouseOver = () => {
+    setShowTooltip(true);
+  };
+
+  const handleMouseOut = () => {
+    setShowTooltip(false);
+  };
+
+  const tooltipStyle = {
+    top: position === 'top' ? '-20px' : '',
+    bottom: position === 'bottom' ? '-20px' : '',
+    left: position === 'left' ? '5px' : '',
+    right: position === 'right' ? '5px' : '',
+  };
+
+  return (
+    <div className="tooltip-container flex justify-center mt-20 p-4 ">
+      <button onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}
+        className="py-2 px-5 text-sm font-medium text-white focus:outline-none bg-white rounded-[11px] border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-300 dark:bg-gray-100 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+        {children}
+      </button>
+      {showTooltip && (
+        <div className="tooltip a " style={tooltipStyle}>
+          {content}
+        </div>
+      )}
+    </div>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Tooltip position="top" content="Thanks for hovering! I'm a tooltip">
+        Hover over me!
+
+      </Tooltip>
+
+      <Tooltip position="bottom" content="Thanks for hovering! I'm a tooltip">
+        Hover over me!
+
+      </Tooltip>
+
+      <Tooltip position="left" content="Thanks for hovering! I'm a tooltip">
+        Hover over me!
+
+      </Tooltip>
+
+      <Tooltip position="right" content="Thanks for hovering! I'm a tooltip">
+        Hover over me!
+      </Tooltip>
     </>
   )
 }
